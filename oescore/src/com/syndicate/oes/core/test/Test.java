c5aaeefb25abb2f51527b952117935b224ccfb58
YasserArafat <mdyasserarafat1@gmail.com>
2015-11-13 07:54:17 +0600

    [refs #3] - Initial gradle structure integrated.

---
+++ oescore/src/com/syndicate/oes/core/test/Test.java
@@ -0,0 +1,21 @@
+package com.syndicate.oes.core.test;
+
+/**
+ * *****************************************************************
+ * Copyright  2015.
+ * All Rights Reserved to
+ * Kona Software Lab Ltd.
+ * Redistribution or Using any part of source code or binary
+ * can not be done without permission of Kona Software Lab Ltd.
+ * *****************************************************************
+ * author: Md. Yasser Arafat
+ * email: yasser_arafat@konasl.com
+ * date: 11/13/2015
+ * time: 7:28 AM
+ * ****************************************************************
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("First print of OES");
+    }
+}
